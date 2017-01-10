490259ff2d90e86a39a487e21614bbce6499d4a7
AlexBemore <artwelbo@gmail.com>
2017-01-10 20:10:06 +0200

    Adding Website

---
+++ gulpfile.js
@@ -0,0 +1,67 @@
+const gulp = require("gulp");
+
+var connect = require("gulp-connect-multi")() ;
+gulp.task("connect", connect.server({
+	root: ["site"],
+	port: 1337,
+	livereload: true,
+	open: {
+		browser: "Google Chrome"
+	}
+
+}));
+
+//templates
+const htmlmin = require('gulp-htmlmin');
+
+gulp.task("templates", function() {
+	return gulp.src('./dev/*.html')
+	    .pipe(htmlmin({collapseWhitespace: true}))
+	    .pipe(gulp.dest('./site/'))
+	    .pipe(connect.reload());
+});
+
+//styles
+const cssmin = require('gulp-cssmin');
+const autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('styles', function () {
+    return gulp.src('./dev/assets/css/*.css')
+        .pipe(cssmin())
+        .pipe(autoprefixer('last 2 versions'))
+        .pipe(gulp.dest('./site/assets/css/'))
+        .pipe(connect.reload());
+});
+
+//js
+var uglify = require('gulp-uglify');
+var pump = require('pump');
+gulp.task('compress', function (cb) {
+  pump([
+        gulp.src('./dev/assets/js/vendor/*.js'),
+        uglify(),
+        gulp.dest('./site/assets/js/vendor/')
+    ],
+    cb
+  );
+});
+
+//images
+const imagemin = require("gulp-imagemin");
+gulp.task("images", function() {
+	return gulp.src("./dev/images/*")
+		.pipe(imagemin())
+		.pipe(gulp.dest("./site/images/"))
+		.pipe(connect.reload());
+});
+
+//watch
+gulp.task("watch", function(){
+	gulp.watch('./dev/*.html', ["templates"]);
+	gulp.watch("./dev/assets/css/*.css", ["styles"]);
+	gulp.watch("./dev/images/*", ["images"]);
+})
+
+
+gulp.task("default", ["templates", "styles", "compress", "images"]);
+gulp.task("dev", ["default", "connect", "watch"]);
